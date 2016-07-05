2168e8683c426657c61d03efe311b242caf1a141
Chris Sanders <astericky@gmail.com>
2016-07-04 20:12:35 -0400

    Add foundation to Main and Example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
+import {Link} from 'react-router';
 
 const Examples = (props) => {
     return (
         <div>
-            <h3>Examples!</h3>
-            <p>Welcome to examples page!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out!</p>
+            <ol>
+                <li>
+                    <Link to='/?location=NewYork'>New York, NY</Link>
+                </li>
+                <li>
+                    <Link to='/?Location=Rio'>Rio, Brazil</Link>
+                </li>
+            </ol>
         </div>
     );
 };
